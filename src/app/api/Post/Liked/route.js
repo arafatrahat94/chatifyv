ddbde6a38b58f19e94aa21f1ef04ae8a147bccc0
Arafath Hossain <arafatrahat94@gmail.com>
2023-12-19 01:20:40 -0800

    again fix

--- src/app/api/Post/Liked/route.js
+++ src/app/api/Post/Liked/route.js
@@ -3,18 +3,20 @@ import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
 const db = DbConnect();
 export const GET = async (req) => {
-  const PostCollection = (await db).collection("post");
-  const userCollection = (await db).collection("users");
-  const searchParams = req.nextUrl.searchParams;
-  const id = searchParams.get("id");
-  console.log(searchParams);
-  const findPost = await PostCollection.findOne({ _id: new ObjectId(id) });
-  const users = await userCollection.find().toArray();
-  const sendingArray = [];
-  findPost?.likes.map((x) => {
-    const filtering = users.find((y) => y.email === x.likerEmail);
-    sendingArray.push(filtering);
-  });
+  if (db) {
+    const PostCollection = (await db).collection("post");
+    const userCollection = (await db).collection("users");
+    const searchParams = req.nextUrl.searchParams;
+    const id = searchParams.get("id");
+    console.log(searchParams);
+    const findPost = await PostCollection.findOne({ _id: new ObjectId(id) });
+    const users = await userCollection.find().toArray();
+    const sendingArray = [];
+    findPost?.likes.map((x) => {
+      const filtering = users.find((y) => y.email === x.likerEmail);
+      sendingArray.push(filtering);
+    });
 
-  return new NextResponse(JSON.stringify(sendingArray));
+    return new NextResponse(JSON.stringify(sendingArray));
+  }
 };
