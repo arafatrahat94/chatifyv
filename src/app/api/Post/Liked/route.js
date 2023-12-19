4916accbef6d626cd0ebf2e167d0644a7f10a02e
Arafath Hossain <arafatrahat94@gmail.com>
2023-12-19 01:15:45 -0800

    update route

--- src/app/api/Post/Liked/route.js
+++ src/app/api/Post/Liked/route.js
@@ -2,9 +2,9 @@ import DbConnect from "@/services/DbConnect";
 import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
 const db = DbConnect();
-const PostCollection = (await db).collection("post");
-const userCollection = (await db).collection("users");
 export const GET = async (req) => {
+  const PostCollection = (await db).collection("post");
+  const userCollection = (await db).collection("users");
   const searchParams = req.nextUrl.searchParams;
   const id = searchParams.get("id");
   console.log(searchParams);
@@ -15,6 +15,6 @@ export const GET = async (req) => {
     const filtering = users.find((y) => y.email === x.likerEmail);
     sendingArray.push(filtering);
   });
-  //   console.log(sendingArray);
+
   return new NextResponse(JSON.stringify(sendingArray));
 };
