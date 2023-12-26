79eb54f79de176391b0ef059331cea638ed3eadf
Arafath Hossain <arafatrahat94@gmail.com>
2023-12-27 01:30:06 +0600

    susupense wrapped the dynamic profile section

--- src/app/(Home)/[id]/page.js
+++ src/app/(Home)/[id]/page.js
@@ -1,10 +1,20 @@
+import { Suspense } from "react";
 import BannerANdProfilePic from "./BannerANdProfilePic";
+import ComponentLoader from "@/Utilities/ComponentLoader";
 
 const DynamicPage = ({ params }) => {
   console.log(params?.id);
   return (
     <div className="mx-[15px] md:mx-[24px]">
-      <BannerANdProfilePic email={params?.id} />
+      <Suspense
+        fallback={
+          <div className="w-full min-h-screen flex items-center justify-center">
+            <ComponentLoader />
+          </div>
+        }
+      >
+        <BannerANdProfilePic email={params?.id} />
+      </Suspense>
     </div>
   );
 };
